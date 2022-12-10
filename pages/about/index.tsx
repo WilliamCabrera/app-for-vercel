3f0ba5414c9f1a63915f043f2e39bc28a7d29fac
William <williamjnr8708@gmail.com>
2022-12-09 20:04:32 -0500

    update

---
+++ pages/about/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+ const About = () => {
+  return (
+	<> 
+	  <h1> about</h1>
+       <div> lorem fdklfkd fdbfd fdfd fd fdufvd fd fd fd;f fdvfd </div>
+	</>
+	 
+  )
+}
+
+export default About;
